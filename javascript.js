dbfb3ede4537db1157d50891fce7fe27c2443a2f
Candid Dauth <cdauth@cdauth.de>
2009-04-26 21:22:39 +0000

    Added license note.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,23 @@
+/*
+	This file is part of cdauth’s map.
+
+	OSM Route Manager is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cdauth’s map is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with cdauth’s map.  If not, see <http://www.gnu.org/licenses/>.
+
+	Obtain the source code from http://svn.cdauth.de/viewvc.cgi/Tools/osm/map/
+	or svn://svn.cdauth.de/tools/osm/map/.
+*/
+
 var map;
 var layerMarkers;
 var layerResults;
--- namefinder.php
+++ namefinder.php
@@ -1,4 +1,24 @@
 <?php
+/*
+	This file is part of cdauth’s map.
+
+	OSM Route Manager is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cdauth’s map is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with cdauth’s map.  If not, see <http://www.gnu.org/licenses/>.
+
+	Obtain the source code from http://svn.cdauth.de/viewvc.cgi/Tools/osm/map/
+	or svn://svn.cdauth.de/tools/osm/map/.
+*/
+
 	$fh = fsockopen("gazetteer.openstreetmap.org", 80);
 	fwrite($fh, "GET /namefinder/search.xml?find=".rawurlencode(isset($_GET["find"]) ? $_GET["find"] : "")." HTTP/1.0\r\n");
 	fwrite($fh, "Host: gazetteer.openstreetmap.org\r\n");
