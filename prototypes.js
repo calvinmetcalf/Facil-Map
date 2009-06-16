94c6fc5099a32faa22e528969a7e1a4efb6286d1
Candid Dauth <cdauth@cdauth.de>
2009-06-16 23:09:37 +0000

    License note still said 'OSM Route Manager'.

--- prototypes.js
+++ prototypes.js
@@ -1,7 +1,7 @@
 /*
 	This file is part of cdauth’s map.
 
-	OSM Route Manager is free software: you can redistribute it and/or modify
+	cdauth’s map is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
