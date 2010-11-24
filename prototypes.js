5e4feefbaf267a7f3ba4d3a70979bcdda4c1f268
Candid Dauth <cdauth@cdauth.eu>
2010-11-24 12:51:37 +0000

    Added Dutch prototypes.js translation by Frank Salet.

--- prototypes.js
+++ prototypes.js
@@ -150,6 +150,57 @@ OpenLayers.Lang.de = OpenLayers.Util.extend(OpenLayers.Lang.de, {
 	"Tags" : "Attribute"
 });
 
+OpenLayers.Lang.nl = OpenLayers.Util.extend(OpenLayers.Lang.nl, {
+	"[Zoom]" : "[Zoom]",
+	"[Remove]" : "[Verwijderen]",
+	"attribution-osm" : "Weergave: ${rendering} (CC-by-SA), Gegevens: <a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a> (CC-by-SA)",
+	"attribution-relief" : "Reliëf: <a href=\"http://hikebikemap.de/\">Hike &amp; Bike Map</a>",
+	"attribution-oom-streets" : "Stratenlaag: <a href=\"http://oobrien.com/oom/\">OpenOrienteeringMap</a> (<a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a>-gegevens, CC-by-SA)",
+	"attribution-oom-labels" : "Bijschriften: <a href=\"http://oobrien.com/oom/\">OpenOrienteeringMap</a> (<a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a>-gegevens, CC-by-SA)",
+	"attribution-hiking" : "Wandelbeschrijvingen: <a href=\"http://osm.lonvia.de/world_hiking.html\">Lonvia's Hiking Map</a>/<a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a>-gegevens, CC-by-SA",
+	"attribution-os-streetview" : "Bevat Ordnance-Survey-gegevens, © Crown copyright and database right 2010",
+	"Create a marker" : "Marker maken",
+	"Coordinates" : "Coördinaten",
+	"unknown" : "onbekend",
+	"Error parsing file." : "Fout bij gegevens ophalen.",
+	"Latitude" : "Latitude (breedte)",
+	"Longitude" : "Longitude (lengte)",
+	"Get directions (OpenRouteService)" : "Route bepalen (OpenRouteService)",
+	"OpenStreetMap Permalink" : "OpenStreetMap Permalink",
+	"OpenStreetMap Shortlink" : "OpenStreetMap Shortlink",
+	"Google Maps Permalink" : "Google Maps Permalink",
+	"Yahoo Maps Permalink" : "Yahoo Maps Permalink",
+	"OpenStreetMap Links" : "OpenStreetMap Links",
+	"Wikimedia GeoHack" : "Wikimedia GeoHack",
+	"Go home" : "Naar voorpagina",
+	"Mapnik" : "Mapnik",
+	"MapSurfer Road" : "MapSurfer Road",
+	"MapSurfer Topographic" : "MapSurfer Topographic",
+	"OpenStreetBrowser" : "OpenStreetBrowser",
+	"Osmarender" : "Osmarender",
+	"OpenCycleMap" : "OpenCycleMap",
+	"Reit- und Wanderkarte" : "Fiets- en wandelkaart",
+	"Hike & Bike Map" : "Hike & Bike Map",
+	"OpenPisteMap" : "OpenPisteMap",
+	"ÖPNV-Karte" : "ÖPNV-Karte",
+	"Google Streets" : "Google Streets",
+	"Google Satellite" : "Google Satelliet",
+	"Google Hybrid" : "Google Hybrid",
+	"Google Terrain" : "Google Terrein",
+	"Google MapMaker" : "Google MapMaker",
+	"Google MapMaker Hybrid" : "Google MapMaker Hybrid",
+	"Yahoo Street" : "Yahoo Street",
+	"Yahoo Satellite" : "Yahoo Satelliet",
+	"Yahoo Hybrid" : "Yahoo Hybrid",
+	"Relief" : "Reliëf",
+	"Hiking symbols" : "Wandelbeschrijvingen",
+	"Coordinate grid" : "Coördinatensysteem",
+	"Izometrická 3D mapa ČR" : "Izometrická 3D mapa ČR",
+	"Streets overlay" : "Stratenlaag",
+	"Labels overlay" : "Bijschriften"
+});
+
+
 OpenLayers.cdauthBackup = { };
 
 // Save parent classes in class objects, needed for makeClassName()
