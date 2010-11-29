c0acfd20ccf9ff4f5d255ed1a075b89f1259cb77
Candid Dauth <cdauth@cdauth.eu>
2010-11-29 21:21:36 +0000

    Using yuicompressor.

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ list_files() {
 files="$(list_files)"
 
 echo -n > facilmap_debug.js
-echo -n > facilmap.js
+echo -n > facilmap_src.js
+rm -f facilmap.js
 
 echo "$files" | while read fname; do
 	echo "document.write(\"<script type=\\\"text/javascript\\\" src=\\\"$fname\\\"></script>\");" >> facilmap_debug.js
@@ -18,5 +19,11 @@ echo "$files" | while read fname; do
 		cat "$fname"
 		echo
 		echo
-	) >> facilmap.js
+	) >> facilmap_src.js
 done
+
+if [ -f yuicompressor-*.jar ]; then
+	java -jar yuicompressor-*.jar facilmap_src.js > facilmap.js
+else
+	ln -s facilmap_src.js facilmap.js
+fi
